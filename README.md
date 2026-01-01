031b90ba2fb5571e4108b85f4678aaa2c92f48d0
Nikhil Kumar Prajapati <np1626978@gmmail.com>
2026-01-01 19:39:28 +0530

    API SETUP

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 first i installed the react vite app
 then i setup the tailwind css for the styling
 installed the required library 
+
+adding providers to main.jsx
+now i am going to setup unspalsh and instant db for images and client side database
+
---
+++ src/api/unsplash.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const unsplash = axios.create({
+    baseURL: "https://api.unsplash.com",
+    headers: {
+        Authorization: `Client-ID ${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`,
+    },
+});
+
+
+export const FetchImages = async ({ pageParam = 1 }) => {
+    try {
+        const res = await unsplash.get("/photos", {
+            params: {
+                page: pageParam,
+                per_page: 12,
+            }
+        });
+        return res.data;
+    } catch (error) {
+        console.error("Error fetching images:", error);
+        throw error;
+    }
+};
